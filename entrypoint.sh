ad70ac457a4d4f11e2000c4c7df97345c8485a4d
Gauravpurohit409 <gauravpurohit409@gmail.com>
2024-06-25 19:48:07 +0530

    Change command sequence.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# Ensure the database is created and migrated
-bundle exec rake db:create db:migrate
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
+# Ensure the database is created and migrated
+bundle exec rake db:create db:migrate
